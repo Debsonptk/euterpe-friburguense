33fbac0ddbf9d6a14ad73a2b1610cb781b8aa42f
DebsonCoelho <debsoncoelh@gmail.com>
2024-02-28 16:11:22 -0300

    changed menu

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -4,6 +4,11 @@ import styled from 'styled-components'
 export const BgMenu = styled.div`
   border-bottom: 2px solid #ccc;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.3);
+  /*position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 1000;
+  background-color: #fff;*/
 `
 
 export const SizeLogo = styled.img`
@@ -22,12 +27,12 @@ export const LinkStyle = styled.div`
 `
 export const StyledIcon = styled(Link)`
   .instagram-icon {
-    font-size: 30px; /* Tamanho padrão */
+    font-size: 30px;
   }
 
   @media (max-width: 768px) {
     .instagram-icon {
-      font-size: 20px; /* Tamanho para telas pequenas (ajuste conforme necessário) */
+      font-size: 20px;
     }
   }
 `
